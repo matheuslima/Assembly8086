ba0ef83db4ace1e960022ee56c173523275abfdc
Matheus <matheuslimaufc@gmail.com>
2012-08-18 21:58:02 -0300

    Corrida tabulação do arquivo Timer8254.asm

--- Timer8254.asm
+++ Timer8254.asm
@@ -3,15 +3,15 @@
 .model small   
 org 100H
 .data
-COUNTER_ONE_ADDRESS	 EQU 80H
-COUNTER_TWO_ADDRESS	 EQU 81H
-COUNTER_TREE_ADDRESS EQU 82H
-CONTROL_WORD_ADDRESS EQU 83H
+COUNTER_ONE_ADDRESS		EQU 80H
+COUNTER_TWO_ADDRESS		EQU 81H
+COUNTER_TREE_ADDRESS 	EQU 82H
+CONTROL_WORD_ADDRESS 	EQU 83H
 
 ; 00 (counter 0) + 11 (escreve LSB antes, MSB depois) + 011 (seleciona modo 3, gerar onda quadrada) + 0 (contador modo binário)
-CONTROL_WORD_VALUE   EQU 00110110b 
+CONTROL_WORD_VALUE   	EQU 00110110b 
 ; 2.5 MHz (sinal de entrada) / 9.6 KHz (sinal de saída) = 260 = 104H
-COUNTER_VALUE        EQU 104H
+COUNTER_VALUE        	EQU 104H
 .code
 
 ;Escreve palavra de controle no contador
